808fce57365a2f0eadb67a1ed53b33d92a2fbad0
gormaniac <83027042+gormaniac@users.noreply.github.com>
2023-06-24 10:39:09 -0600

    feat: add svc evts

--- src/yarastorm/defs.py
+++ src/yarastorm/defs.py
@@ -10,15 +10,15 @@ SVC_VER = __version__
 SVC_GUID = "12bc0c8c1a37a29808d97972875d3913"
 SVC_SYN_MIN_VER = (2, 137, 0)
 
-SVC_EVTS = {}
+SVC_EVTS = {
+    "add": {
+        "storm": f'[(meta:source={SVC_GUID} :name={SVC_NAME})]'
+    }
+}
 
 
 class GormoYaraPkg(StormPkg):
     """The gormo.yara Storm package for the yarastorm service."""
 
-    pkg_name = SVC_NAME
-    pkg_ver = SVC_VER
-    synapse_minversion = SVC_SYN_MIN_VER
 
-
-PKGDEFS = (GormoYaraPkg(),)
+PKGDEFS = (GormoYaraPkg(proto_name="gormo.yara").asdict(),)
