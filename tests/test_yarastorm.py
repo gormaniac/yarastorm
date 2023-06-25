a7b0d81a46d09636f2beee1f4f26bf55923a965a
gormaniac <83027042+gormaniac@users.noreply.github.com>
2023-06-24 23:29:36 -0600

    test: update yara scan test

--- tests/test_yarastorm.py
+++ tests/test_yarastorm.py
@@ -47,7 +47,6 @@ class TestYaraStorm(s_tests.SynTest):
 
     async def test_cmd_yara_check(self):
         async with self.getTestCoreProxSvc(YaraSvc) as (core, prox, svc):
-            # Not really how we will use this - just testing right now.
             await core.nodes('[ it:app:yara:rule=* :text="test" :name="rule1"]')
             await core.nodes('[ it:app:yara:rule=* :text="rule dummy { condition: true }" :name="dummy" :enabled=$lib.true]')
             msgs = await core.stormlist(f"it:app:yara:rule | gormo.yara.check")
@@ -55,19 +54,19 @@ class TestYaraStorm(s_tests.SynTest):
             self.stormHasNoErr(msgs)
 
     async def test_cmd_yara_scan(self):
-        async with self.getTestCoreProxSvc(YaraSvc) as (core, prox, svc):
-            # Not really how we will use this - just testing right now.
-            await core.nodes('[ it:app:yara:rule=* :text="test" :name="rule1"]')
-            await core.nodes('[ it:app:yara:rule=* :text="rule dummy { condition: true }" :name="dummy" :enabled=$lib.true]')
-            await core.nodes('[ file:bytes=* ]')
-            await core.nodes('[ file:bytes=37268335dd6931045bdcdf92623ff819a64244b53d0e746d438797349d4da578 ]')
-            msgs = await core.stormlist(f"file:bytes | gormo.yara.scan")
-            print(msgs)
-            self.stormHasNoErr(msgs)
+        async with self.getTestAxon() as axon:
+            axon.put(b"test")
+            async with self.getTestCoreProxSvc(YaraSvc) as (core, prox, svc):
+                await core.nodes('[ it:app:yara:rule=* :text="test" :name="rule1"]')
+                await core.nodes('[ it:app:yara:rule=* :text="rule dummy { condition: true }" :name="dummy" :enabled=$lib.true]')
+                await core.nodes('[ file:bytes=* ]')
+                await core.nodes('[ file:bytes=37268335dd6931045bdcdf92623ff819a64244b53d0e746d438797349d4da578 ]')
+                msgs = await core.stormlist(f"file:bytes | gormo.yara.scan")
+                print(msgs)
+                self.stormHasNoErr(msgs)
 
     async def test_cmd_yara_enable(self):
         async with self.getTestCoreProxSvc(YaraSvc) as (core, prox, svc):
-            # Not really how we will use this - just testing right now.
             rule_id = s_common.guid()
             await core.nodes(f'[ it:app:yara:rule={rule_id} :text="test" :name="rule1"]')
             msgs = await core.stormlist(f"it:app:yara:rule={rule_id} | gormo.yara.enable")
@@ -76,7 +75,6 @@ class TestYaraStorm(s_tests.SynTest):
 
     async def test_cmd_yara_disable(self):
         async with self.getTestCoreProxSvc(YaraSvc) as (core, prox, svc):
-            # Not really how we will use this - just testing right now.
             rule_id = s_common.guid()
             await core.nodes(f'[ it:app:yara:rule={rule_id} :text="rule dummy {{ condition: true }}" :name="dummy" :enabled=$lib.true]')
             msgs = await core.stormlist(f"it:app:yara:rule={rule_id} | gormo.yara.disable")
