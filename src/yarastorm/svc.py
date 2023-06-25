a5c89ac470759e2a6c22030c37d06699f384a211
gormaniac <83027042+gormaniac@users.noreply.github.com>
2023-06-24 23:24:19 -0600

    feat: call the python-yara methods in yarasvc

--- src/yarastorm/svc.py
+++ src/yarastorm/svc.py
@@ -1,11 +1,15 @@
 """The yarastorm service."""
 
 
+import binascii
 import os
 
-from stormlibpp import TelepathRetn
+from stormlibpp import s_exc
 from stormlibpp.telepath import BoolRetn
+import synapse.exc as s_exc
 import synapse.lib.cell as s_cell
+import synapse.telepath as s_telepath
+import yara
 
 from .api import YaraApi
 
@@ -36,17 +40,54 @@ class YaraSvc(s_cell.Cell):
             os.path.join(self.dirn, self.conf.get("rule_dir"))
         )
 
-    async def test(self, test):
+    async def _getBytes(self, sha256: str) -> bytes | None:
+        buffer = b""
 
-        return {"status": True, "data": 'test', "mesg": 'a message'}
+        async with s_telepath.withTeleEnv():
+            async with await s_telepath.openurl(self.axon_url) as axon:
+                try:
+                    async for part in axon.get(binascii.unhexlify(sha256)):
+                        buffer += part
+                except s_exc.NoSuchFile:
+                    return None
+
+        if len(buffer) == 0:
+            return None
+
+        return buffer
 
     async def matchFile(self, file_sha256: str, yara_rule) -> BoolRetn:
         """Test if a Yara rule matches a given file in the Axon."""
 
-        retn = BoolRetn(status=True, mesg="", data=True)
-        return retn
+        file_bytes = await self._getBytes(file_sha256)
+
+        if file_bytes is None:
+            return BoolRetn(
+                status=False,
+                mesg=f"Unable to find bytes for {file_sha256}",
+                data=False
+            )
+
+        # TODO read the compiled yara file from disk if up to date
+        rule = yara.compile(source=yara_rule, error_on_warning=True)
+
+        if rule.match(file_bytes):
+            return BoolRetn(status=True, mesg="", data=True)
+
+        return BoolRetn(status=True, mesg="", data=False)
 
     async def compileRule(self, yara_rule, check: bool = False) -> BoolRetn:
         """Compile the given Yara rule and save it to this Cell's storage."""
 
-        return TelepathRetn(status=True, mesg="", data=yara_rule)
+        try:
+            rule = yara.compile(source=yara_rule, error_on_warning=True)
+        except yara.SyntaxError as err:
+            return BoolRetn(status=False, mesg=f"Yara Syntax Error - {err}", data=False)
+        except yara.Error as err:
+            return BoolRetn(status=False, mesg=f"Yara Error - {err}", data=False)
+
+        if check:
+            return BoolRetn(status=True, mesg="Successfully compiled rule!", data=True)
+
+        # TODO actually save the compiled rule if check is false
+        return BoolRetn(status=True, mesg="", data=True)
